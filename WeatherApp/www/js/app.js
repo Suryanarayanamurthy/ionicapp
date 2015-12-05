21608af177d5c99047a002889fe824faed90cb30
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-05 20:06:21 +0100

    removing reduntent code-1

--- WeatherApp/www/js/app.js
+++ WeatherApp/www/js/app.js
@@ -28,23 +28,23 @@ function initialize() {
         var map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
         
-        //Marker + infowindow + angularjs compiled ng-click
-        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-        var compiled = $compile(contentString)($scope);
-
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
-
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
+//        //Marker + infowindow + angularjs compiled ng-click
+//        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+//        var compiled = $compile(contentString)($scope);
+//
+//        var infowindow = new google.maps.InfoWindow({
+//          content: compiled[0]
+//        });
+//
+//        var marker = new google.maps.Marker({
+//          position: myLatlng,
+//          map: map,
+//          title: 'Uluru (Ayers Rock)'
+//        });
+//
+//        google.maps.event.addListener(marker, 'click', function() {
+//          infowindow.open(map,marker);
+//        });
 
         $scope.map = map;
       }
@@ -52,6 +52,7 @@ function initialize() {
       
       $scope.centerOnMe = function() {
         if(!$scope.map) {
+                
           return;
         }
 
