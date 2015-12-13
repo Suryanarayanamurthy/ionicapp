c7008c1561edf5f71942f3631fa8b81bf15cf92e
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-13 23:52:16 +0100

    kickoff ionic apps

--- pomodoro/www/src/pomodoroApp.js
+++ pomodoro/www/src/pomodoroApp.js
@@ -1,5 +1,25 @@
 
-var app = angular.module('PomodoroApp', ['ngRoute']);
+var app = angular.module('PomodoroApp', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+function ContentController($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+}
+
 app.controller("AppController", function($scope, $interval, $timeout) {
   $scope.breaktime =5;
   $scope.worktime =25;
