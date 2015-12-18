dd71104e7ef8da52983541c098a7f55d119f90b1
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-18 02:16:01 +0100

    fixed the settings bug
    the local expression in the html is taking high priority than the $scope
    once in controller.

--- pomodoro/www/js/controllers.js
+++ pomodoro/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('PomodoroApp.controllers', [])
+var app = angular.module('PomodoroApp.controllers', [])
 
 //.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 //
@@ -41,26 +41,32 @@ angular.module('PomodoroApp.controllers', [])
 //  };
 //})
 
-.controller("clockCtrl", function($scope, $interval, $timeout) {
+
+
+app.controller("clockCtrl", function($scope, $interval, $timeout) {
+  //initial default value of all the parameters you see on the page.
   $scope.breaktime =5;
   $scope.worktime =25;
   $scope.longBreaktime =15;
   $scope.minutes=25;
   $scope.seconds=0;
   $scope.pomoNum=1;
-
-  var timeLeft = $scope.worktime * 60;
   var secession = "work";
+  var timeLeft = $scope.worktime * 60;
   var promise;
 
+    //source for the audio track, to notifification when the counddown reaches 0.
+    // TODO: make the resourse local.
   var wav = 'http://www.oringz.com/oringz-uploads/sounds-917-communication-channel.mp3';
   var audio = new Audio(wav);
 
-
+    // called using a promise todo the countdown on the screen, here all the behaviour of the app is done.
+    // called this functiontion irrespective of the type of secession we are in.
   function ShowTime(){
     $scope.minutes = Math.floor(timeLeft / 60);
     $scope.seconds = timeLeft - ($scope.minutes * 60);
     timeLeft -= 1;
+      //logic when a work secession is over.
     if(secession == "work" && timeLeft <= 0)
     {
         $scope.pomoNum++;
@@ -75,12 +81,14 @@ angular.module('PomodoroApp.controllers', [])
             }
         audio.play();
     }
+      //logic when a break(aka play) secession is over
     else if(secession == "play"  && timeLeft <= 0)
     {
         secession = "work";
         timeLeft = $scope.worktime * 60;
         audio.play();
     }
+      // logic when a longbreak(asks playHard) secession is over
       else if( secession =="playHard" && timeLeft <= 0)
           {
               secession = "work";
@@ -90,13 +98,21 @@ angular.module('PomodoroApp.controllers', [])
           }
 };
 
+    // when play button is clicked call the showTime for every once second, 
+    // irrespective of the secession we are in right now,
+    //because the logic for handiling behaviour during each secession is on showtime function.
   $scope.play = function() {
     promise = $interval(ShowTime,1000,0);
   };
+    // cancel the promise when pause button is clicked, the play will continue from where we left of 
+    //becuase the current secession and the time where we paused is in the variables "timeLeft" and "secession".
   $scope.pause = function()
   {
      $interval.cancel(promise);
   };
+    // reset all the variables to default;
+    // work = 25mins, break = 5 mins, long break = 15 mins and secession = work.
+    // and cancel the existing promise.
   $scope.reset = function()
   {
   $interval.cancel(promise);
@@ -107,22 +123,30 @@ angular.module('PomodoroApp.controllers', [])
   timeLeft = $scope.worktime * 60;
   secession = "work";
   }
-  $scope.workUpdated = function()
+  
+  /* updates the default time values for each secession*/
+  
+  $scope.workUpdated = function(worktime)
   {
+      $scope.worktime = worktime;
   if(secession == "work")
+      
   timeLeft = $scope.worktime*60;
 
   if($scope.worktime < 0) $scope.worktime = 0;
   }
-  $scope.playUpdated = function()
+  $scope.playUpdated = function(breaktime)
   {
+      $scope.breaktime = breaktime;
     if(secession == "play")
     timeLeft = $scope.breaktime *60;
 
     if($scope.breaktime < 0 ) $scope.breaktime =0;
+      console.log($scope.breaktime);
   }
-  $scope.playHardUpdated = function()
+  $scope.playHardUpdated = function(longBreaktime)
   {
+      $scope.longBreaktime = longBreaktime;
       if(secession == "playHard")
           timeLeft =$scope.longBreaktime *60;
       
