757c3f446aa0ece7a86c1da00da138196ae086ff
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-12-23 03:42:05 +0100

    # 48 partially done.

--- pomodoro/www/js/app.js
+++ pomodoro/www/js/app.js
@@ -3,24 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('PomodoroApp', ['ionic', 'PomodoroApp.controllers'])
+angular.module('PomodoroApp', ['ionic','ionic.service.core','PomodoroApp.services' ,'PomodoroApp.controllers'])
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-
-
-app.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
     .state('app', {
@@ -34,7 +19,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
     url: '/karma',
     views: {
       'menuContent': {
-        templateUrl: 'templates/karma.html'
+        templateUrl: 'templates/karma.html',
+          //controller: 'karmaCtrl'
       }
     }
   })
@@ -79,4 +65,42 @@ app.config(function($stateProvider, $urlRouterProvider) {
 //  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/clock');
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 });
+
+angular.module('PomodoroApp.services', [])
+  .factory('ListFactory', function() {
+
+    var list = [];
+    var listStore = localStorage.getItem("list");
+    if (listStore != null && listStore != '' && angular.isArray(angular.fromJson(listStore))) {
+      list = angular.fromJson(listStore);
+    }
+    var listSrv = {
+      setList: function(newList) {
+        list = newList;
+        localStorage.setItem("list", angular.toJson(list));
+        return true;
+      },
+      getList: function() {
+        if (list != null) {
+          return list;
+        } else {
+          return [];
+        }
+      }
+    };
+    return listSrv;
+  });
